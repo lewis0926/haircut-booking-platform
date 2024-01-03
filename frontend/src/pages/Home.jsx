b1e44b99b9ffdbd6578dfad7b627b48b12feb622
Lewis Shum <lewishum96@gmail.com>
2024-01-03 15:22:48 -0500

    Init Stylist Page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 
 import Header from '../partials/Header';
 import HeroHome from '../partials/HeroHome';
-import FeaturesHome from '../partials/Features';
-import FeaturesBlocks from '../partials/FeaturesBlocks';
 import Testimonials from '../partials/Testimonials';
 import Newsletter from '../partials/Newsletter';
 import Footer from '../partials/Footer';
 import Banner from '../partials/Banner';
+import StylistSection from "../partials/StylistSection";
 
 function Home() {
   return (
@@ -22,7 +21,8 @@ function Home() {
         {/*  Page sections */}
         <HeroHome />
         {/* <FeaturesHome /> */}
-        <FeaturesBlocks />
+        {/* Section header */}
+        <StylistSection />
         <Testimonials />
         <Newsletter />
 
